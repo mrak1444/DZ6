d496151d3e3441c16858080c7e3734c77bc587ef
Mrak1444 <Mrak1444@mail.ru>
2020-11-24 14:07:11 +0600

    Убрал лишнее

--- DZ6/Task1.cs
+++ DZ6/Task1.cs
@@ -3,15 +3,12 @@
 Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
 */
 using System;
-// Описываем делегат. В делегате описывается сигнатура методов, на
-// которые он сможет ссылаться в дальнейшем (хранить в себе)
+
 public delegate double Fun(double x, double a);
 
 class Task1
 {
-    // Создаем метод, который принимает делегат
-    // На практике этот метод сможет принимать любой метод
-    // с такой же сигнатурой, как у делегата
+
     public static void Table(Fun F, double x, double a, double b)
     {
         Console.WriteLine("----- X ----- Y -----");
@@ -22,7 +19,7 @@ class Task1
         }
         Console.WriteLine("---------------------");
     }
-    // Создаем метод для передачи его в качестве параметра в Table
+
     public static double MyFunc(double x, double a)
     {
         return a * (x * x);
@@ -34,11 +31,9 @@ class Task1
 
     public void Run()
     {
-        // Создаем новый делегат и передаем ссылку на него в метод Table
         Console.WriteLine("Таблица функции a*x^2:");
-        // Параметры метода и тип возвращаемого значения, должны совпадать с делегатом
         Table(new Fun(MyFunc), -2, 2, 2);
         Console.WriteLine("Таблица функции a*sin(x)");
-        Table(MySin, -2, 1, 2);      // Можно передавать уже созданные методы
+        Table(MySin, -2, 1, 2);
     }
 }
